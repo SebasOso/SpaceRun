4cfa60875e045f92227372cb7d9d64d8c8af729c
SebasOso <83346917+SebasOso@users.noreply.github.com>
2023-05-21 15:47:50 -0500

    a

--- Assets/RobotManager.cs
+++ Assets/RobotManager.cs
@@ -1,8 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
-using static UnityEditor.ShaderData;
 
 public class RobotManager : MonoBehaviour
 {
