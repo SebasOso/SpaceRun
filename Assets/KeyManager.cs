9f0d35f5ce8094326bd7d54d775c2bf818f36997
SebasOso <83346917+SebasOso@users.noreply.github.com>
2023-05-22 12:06:10 -0500

    a

--- Assets/KeyManager.cs
+++ Assets/KeyManager.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class KeyManager : MonoBehaviour
 {
     [SerializeField] private GameObject lightDoor;
+    [SerializeField] private GameObject screen;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Robot"))
         {
             lightDoor.SetActive(true);
+            screen.SetActive(false);
         }
     }
 }
