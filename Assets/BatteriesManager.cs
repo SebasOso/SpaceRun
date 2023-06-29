6806c600e4e7a1811114adf54e2e5153b64506a9
Sebastian Osorio <83346917+SebasOso@users.noreply.github.com>
2023-06-29 16:36:28 -0500

    BugFixedGravityGun

--- Assets/BatteriesManager.cs
+++ Assets/BatteriesManager.cs
@@ -10,11 +10,19 @@ public class BatteriesManager : MonoBehaviour
     [SerializeField] private bool snapTwo = false;
     [SerializeField] private bool snapThree = false;
     [SerializeField] private GameObject palanca;
+    [SerializeField] private InteractableFacade gravityGun;
+    private void Start()
+    {
+        snapOne = false;
+        snapTwo = false;
+        snapThree = false;
+    }
     void Update()
     {
         if (snapOne == true && snapTwo == true && snapThree == true)
         {
             palanca.SetActive(true);
+            gravityGun.EnableGrabReceiver();
         }
         else
         {
